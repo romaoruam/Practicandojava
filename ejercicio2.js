d81cb65bef220d177a83de462c3e64feef8e131e
Mauro Romano <167482131+romaoruam@users.noreply.github.com>
2024-06-13 11:04:37 +0000

    realice ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+const ciudad1 = "tapiales";
+const ciudad2 = "madero";
+const ciudad3 = "tablada";
+const ciudad4 = "celina";
+const ciudad5 = "bonzi";
+
+console.log ("se dijo " + ciudad1);
+console.log (" la ciudad vecina es " + ciudad2);
+console.log ("la otra lindera " + ciudad3 + " " + ciudad4 + " " + ciudad5 );

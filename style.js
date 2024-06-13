dc9700ab84fcbb1974e42fc4910f2d8a89053de4
Mauro Romano <roma.letitbleed@hotmail.com>
2024-06-12 22:56:38 -0300

    realice primer ejercicio complementarioJs

---
+++ style.js
@@ -0,0 +1,6 @@
+// Actividad 1 - de ejercicios complementarios
+let nombre = prompt ("Ingrese su nombre");
+let edad = prompt ("ingrese su edad");
+let apellido = prompt ("ingrese su apellido");
+console.log (nombre, edad, apellido);
+alert ("Hola "+ nombre + " " + apellido + " tu edad es " + edad);

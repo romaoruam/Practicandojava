9c8a4594d693032e3220410e186cd41025024e09
Mauro Romano <roma.letitbleed@hotmail.com>
2024-06-13 23:21:52 -0300

    Agregue carpeta js e inclui archivos js

---
+++ .js/ejercicio3.js
@@ -0,0 +1,8 @@
+let nombre = "Rogelio";
+let segundonombre = "Facundo";
+let apellido = "Santos";
+let domicilio = "Pasaje Santa Rita 31 - Villa Fiorito";
+let pais = "Paraguay";
+
+let carnet = (nombre," ", segundonombre, " ", apellido,"\n", domicilio, "\n", pais  );
+alert(carnet);
--- ejercicio2.js
+++
@@ -1,9 +0,0 @@
-const ciudad1 = "tapiales";
-const ciudad2 = "madero";
-const ciudad3 = "tablada";
-const ciudad4 = "celina";
-const ciudad5 = "bonzi";
-
-console.log ("se dijo " + ciudad1);
-console.log (" la ciudad vecina es " + ciudad2);
-console.log ("la otra lindera " + ciudad3 + " " + ciudad4 + " " + ciudad5 );

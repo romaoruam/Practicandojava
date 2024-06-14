283adbc7e76385eb6b372d642adbf8c5c892029d
Mauro Romano <roma.letitbleed@hotmail.com>
2024-06-13 23:55:58 -0300

    modifique ubicacion de js y elmine la carpeta

--- .js/ejercicio3.js
+++
@@ -1,8 +0,0 @@
-let nombre = "Rogelio";
-let segundonombre = "Facundo";
-let apellido = "Santos";
-let domicilio = "Pasaje Santa Rita 31 - Villa Fiorito";
-let pais = "Paraguay";
-
-let carnet = (nombre," ", segundonombre, " ", apellido,"\n", domicilio, "\n", pais  );
-alert(carnet);
---
+++ ejercicio3.js
@@ -0,0 +1,10 @@
+let nombre = "Rogelio";
+let segundonombre = "Facundo";
+let apellido = "Santos";
+let domicilio = "Pasaje Santa Rita 31 - Villa Fiorito";
+let pais = "Paraguay";
+
+let carnet = "nombre:" + nombre + " " + "segundonombre:" + segundonombre + " " + "apellido:" + apellido + "\n" + "domicilio:" + domicilio + "\n" + "pais:" + pais;
+
+console.log (carnet);
+alert (carnet);

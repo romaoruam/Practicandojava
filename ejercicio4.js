554233d04ce14c85f718a35111e2f8f950931be8
Mauro Romano <167482131+romaoruam@users.noreply.github.com>
2024-06-14 08:32:45 -0300

    agregue ejercicio 4

---
+++ ejercicio4.js
@@ -0,0 +1,7 @@
+let nombre1 = prompt("Ingrese nombre del Padre");
+let nombre2 = prompt("ingrese nombre de la Madre");
+let nombre3 = prompt("ingrese nombre del Hijo");
+let nombre4 = prompt("ingrese nombre de la Hija");
+
+console.log("el padre de la familia es: " + nombre1+ "\n" + "la madre de la familia es: " + nombre2 + "\n" + "el hijo de la familia es: " + nombre3 + "\n" + "la hija de la familia es: " + nombre4);
+alert("el padre de la familia es: " + nombre1 + "\n " + "la madre de la familia es: " + nombre2 + "\n" + "el hijo de la familia es: " + nombre3 + "\n" + "la hija de la familia es: " + nombre4);

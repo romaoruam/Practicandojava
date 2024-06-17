914a16a2d9d235c9110e46bdb2e9d42b630f4d6a
Mauro Romano <roma.letitbleed@hotmail.com>
2024-06-17 14:27:10 -0300

    Realice ejercicio 6

---
+++ ejercicio6.js
@@ -0,0 +1,11 @@
+let nombreRegistrado = "ROMA";
+let nombre;
+
+do { 
+    nombre = prompt("Ingrese su nombre: ").toUpperCase();  // Convertir el nombre ingresado a mayúsculas
+    if (nombre === nombreRegistrado) {
+        alert("Fui Yo cara de verga");
+    } else {
+        alert("No fui yo, CHUPALA");
+    }
+} while (nombre !== nombreRegistrado);
